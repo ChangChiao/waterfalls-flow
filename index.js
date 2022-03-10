62fe8ab6b794d458bd52940fc1bfb22e62820839
joe <joe.chang1014@gmail.com>
2022-03-10 14:11:03 +0800

    feat add animation

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 window.onload = function () {
   const imageList = [
-    "1 - ArsdCGv.jpg",
-    "2 - LNUXzZF.jpg",
+    "6 - OkKwWM9.jpg",
     "3 - kgTEVHZ.jpg",
     "4 - vZoqs0P.jpg",
-    "5 - SEu9pHo.jpg",
-    "6 - OkKwWM9.jpg",
     "7 - eUppgFB.jpg",
+    "1 - ArsdCGv.jpg",
+    "2 - LNUXzZF.jpg",
+    "5 - SEu9pHo.jpg",
     "8 - WAIXTXs.jpg",
-    "9 - vb0uajo.jpg",
     "10 - s6w2RpD.jpg",
+    "9 - vb0uajo.jpg",
   ];
   const bricklayer = new Bricklayer(document.querySelector(".bricklayer"));
-
   for (let i = 0; i < imageList.length; i++) {
+    const random = Math.random().toString().slice(-6);
     const img = document.createElement("img");
-    img.src = `./images/${imageList[i]}` ;
+    img.src = `./images/${imageList[i]}?${random}`;
+    img.onload = function () {
+      img.className = `animate__animated animate__bounceIn`;
+      img.style.animationDelay = `${0.3 * i}s`;
+    };
     bricklayer.append(img);
   }
-
+  bricklayer.redraw()
 };

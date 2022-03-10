8ac733f6f826d7a6cd465a7a4b81cb0eff2e5a2a
joe <joe.chang1014@gmail.com>
2022-03-10 10:42:44 +0800

    feat bricklayer

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-window.onload = function() {
-   
+window.onload = function () {
+  const imageList = [
+    "1 - ArsdCGv.jpg",
+    "2 - LNUXzZF.jpg",
+    "3 - kgTEVHZ.jpg",
+    "4 - vZoqs0P.jpg",
+    "5 - SEu9pHo.jpg",
+    "6 - OkKwWM9.jpg",
+    "7 - eUppgFB.jpg",
+    "8 - WAIXTXs.jpg",
+    "9 - vb0uajo.jpg",
+    "10 - s6w2RpD.jpg",
+  ];
+  const bricklayer = new Bricklayer(document.querySelector(".bricklayer"));
+
+  for (let i = 0; i < imageList.length; i++) {
+    const img = document.createElement("img");
+    img.src = `./images/${imageList[i]}` ;
+    bricklayer.append(img);
+  }
+
 };
